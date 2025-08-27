7857474fbaa22804666597976349f047dd3e6b48
Sentinal-idea <sinchanakulkarni4749@gmail.com>
2025-08-27 22:13:24 +0530

    Update dashboard.js

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -1,10 +1,4 @@
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
    apiKey: "AIzaSyCDvfrPIcYO2B0YyGLqSgoALR68S8vFTrM",
    authDomain: "striker-e07d7.firebaseapp.com",
    projectId: "striker-e07d7",
